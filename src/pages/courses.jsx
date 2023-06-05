9753645d4976ab86803976c7e1dcf52d8b08a861
Aaron Aragon <aaron.david.nagore@gmail.com>
2023-06-05 21:47:38 +0200

    feat(courses): Se ha completado la carga dinámica de los cursos desde la base de datos.

--- src/pages/courses.jsx
+++ src/pages/courses.jsx
@@ -11,10 +11,23 @@ import {
   HeartIcon
 } from '@heroicons/react/24/solid'
 import { Footer } from '@/widgets/layout'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export function Courses () {
   // const myImage = new CloudinaryImage('sample', { cloudName: 'dpew4mitl' }).resize(fill().width(100).height(150))
 
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('http://localhost:3000/api/courses') // Reemplaza 'API_URL' con la URL de tu API
+      console.log(result)
+      setData(result.data)
+    }
+
+    fetchData()
+  }, [])
   return (
     <>
       <section className='relative block h-[50vh]'>
@@ -27,260 +40,49 @@ export function Courses () {
             <div className='px-20'>
               <div className='my-15 text-center'>
                 <div className='my-8 text-center flex justify-center gap-7 flex-wrap'>
-                  <Card className='w-full max-w-[22rem] max-h-[35rem] overflow-auto shadow-lg'>
-                    <CardHeader floated={false} color='blue-gray'>
-                      <img
-                        src='https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-                        alt='ui/ux review check'
-                      />
-                      <div className='to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 ' />
-                      <IconButton
-                        size='sm'
-                        color='red'
-                        variant='text'
-                        className='!absolute top-4 right-4 rounded-full'
-                      >
-                        <HeartIcon className='h-6 w-6' />
-                      </IconButton>
-                    </CardHeader>
-                    <CardBody>
-                      <div className='mb-3 flex items-center justify-between'>
-                        <Typography variant='h5' color='blue-gray' className='font-medium'>
-                          Wooden House, Florida
-                        </Typography>
-                        <Typography
-                          color='blue-gray'
-                          className='flex items-center gap-1.5 font-normal'
+                  {data.map(item => (
+                    <Card className='w-full max-w-[22rem] h-[40rem] flex flex-col overflow-auto shadow-lg' key={item.id}>
+                      <CardHeader floated={false} color='blue-gray'>
+                        <img
+                          src={item.image}
+                          alt={item.alt}
+                        />
+                        <div className='to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 ' />
+                        <IconButton
+                          size='sm'
+                          color='red'
+                          variant='text'
+                          className='!absolute top-4 right-4 rounded-full'
                         >
-                          <StarIcon className='-mt-0.5 h-5 w-5 text-yellow-700' />
-                          5.0
+                          <HeartIcon className='h-6 w-6' />
+                        </IconButton>
+                      </CardHeader>
+                      <CardBody className='flex-grow'>
+                        <div className='mb-3 flex items-center justify-between'>
+                          <Typography variant='h5' color='blue-gray' className='font-medium text-center'>
+                            {item.title}
+                          </Typography>
+                          <Typography
+                            color='blue-gray'
+                            className='flex items-center gap-1.5 font-normal'
+                          >
+                            <StarIcon className='-mt-0.5 h-5 w-5 text-yellow-700' />
+                            5
+                          </Typography>
+                        </div>
+                        <Typography color='gray'>
+                          {item.description}
                         </Typography>
-                      </div>
-                      <Typography color='gray'>
-                        Enter a freshly updated and thoughtfully furnished peaceful home
-                        surrounded by ancient trees, stone walls, and open meadows.
-                      </Typography>
-
-                    </CardBody>
-                    <CardFooter className='pt-3'>
-                      <Button size='lg' fullWidth>
-                        Reserve
-                      </Button>
-                    </CardFooter>
-                  </Card>
-
-                  <Card className='w-full max-w-[22rem] max-h-[35rem] overflow-auto shadow-lg'>
-                    <CardHeader floated={false} color='blue-gray'>
-                      <img
-                        src='https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-                        alt='ui/ux review check'
-                      />
-                      <div className='to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 ' />
-                      <IconButton
-                        size='sm'
-                        color='red'
-                        variant='text'
-                        className='!absolute top-4 right-4 rounded-full'
-                      >
-                        <HeartIcon className='h-6 w-6' />
-                      </IconButton>
-                    </CardHeader>
-                    <CardBody>
-                      <div className='mb-3 flex items-center justify-between'>
-                        <Typography variant='h5' color='blue-gray' className='font-medium'>
-                          Wooden House, Florida
-                        </Typography>
-                        <Typography
-                          color='blue-gray'
-                          className='flex items-center gap-1.5 font-normal'
-                        >
-                          <StarIcon className='-mt-0.5 h-5 w-5 text-yellow-700' />
-                          5.0
-                        </Typography>
-                      </div>
-                      <Typography color='gray'>
-                        Enter a freshly updated and thoughtfully furnished peaceful home
-                        surrounded by ancient trees, stone walls, and open meadows.
-                      </Typography>
-
-                    </CardBody>
-                    <CardFooter className='pt-3'>
-                      <Button size='lg' fullWidth>
-                        Reserve
-                      </Button>
-                    </CardFooter>
-                  </Card>
-
-                  <Card className='w-full max-w-[22rem] max-h-[35rem] overflow-auto shadow-lg'>
-                    <CardHeader floated={false} color='blue-gray'>
-                      <img
-                        src='https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-                        alt='ui/ux review check'
-                      />
-                      <div className='to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 ' />
-                      <IconButton
-                        size='sm'
-                        color='red'
-                        variant='text'
-                        className='!absolute top-4 right-4 rounded-full'
-                      >
-                        <HeartIcon className='h-6 w-6' />
-                      </IconButton>
-                    </CardHeader>
-                    <CardBody>
-                      <div className='mb-3 flex items-center justify-between'>
-                        <Typography variant='h5' color='blue-gray' className='font-medium'>
-                          Wooden House, Florida
-                        </Typography>
-                        <Typography
-                          color='blue-gray'
-                          className='flex items-center gap-1.5 font-normal'
-                        >
-                          <StarIcon className='-mt-0.5 h-5 w-5 text-yellow-700' />
-                          5.0
-                        </Typography>
-                      </div>
-                      <Typography color='gray'>
-                        Enter a freshly updated and thoughtfully furnished peaceful home
-                        surrounded by ancient trees, stone walls, and open meadows.
-                      </Typography>
-
-                    </CardBody>
-                    <CardFooter className='pt-3'>
-                      <Button size='lg' fullWidth>
-                        Reserve
-                      </Button>
-                    </CardFooter>
-                  </Card>
+                      </CardBody>
+                      <CardFooter className='pt-3 mt-auto'>
+                        <Button size='lg' fullWidth>
+                          Reserve
+                        </Button>
+                      </CardFooter>
+                    </Card>
+                  ))}
                 </div>
 
-                <div className='my-8 text-center flex justify-center gap-7 flex-wrap'>
-                  <Card className='w-full max-w-[22rem] max-h-[35rem] overflow-auto shadow-lg'>
-                    <CardHeader floated={false} color='blue-gray'>
-                      <img
-                        src='https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-                        alt='ui/ux review check'
-                      />
-                      <div className='to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 ' />
-                      <IconButton
-                        size='sm'
-                        color='red'
-                        variant='text'
-                        className='!absolute top-4 right-4 rounded-full'
-                      >
-                        <HeartIcon className='h-6 w-6' />
-                      </IconButton>
-                    </CardHeader>
-                    <CardBody>
-                      <div className='mb-3 flex items-center justify-between'>
-                        <Typography variant='h5' color='blue-gray' className='font-medium'>
-                          Wooden House, Florida
-                        </Typography>
-                        <Typography
-                          color='blue-gray'
-                          className='flex items-center gap-1.5 font-normal'
-                        >
-                          <StarIcon className='-mt-0.5 h-5 w-5 text-yellow-700' />
-                          5.0
-                        </Typography>
-                      </div>
-                      <Typography color='gray'>
-                        Enter a freshly updated and thoughtfully furnished peaceful home
-                        surrounded by ancient trees, stone walls, and open meadows.
-                      </Typography>
-
-                    </CardBody>
-                    <CardFooter className='pt-3'>
-                      <Button size='lg' fullWidth>
-                        Reserve
-                      </Button>
-                    </CardFooter>
-                  </Card>
-
-                  <Card className='w-full max-w-[22rem] max-h-[35rem] overflow-auto shadow-lg'>
-                    <CardHeader floated={false} color='blue-gray'>
-                      <img
-                        src='https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-                        alt='ui/ux review check'
-                      />
-                      <div className='to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 ' />
-                      <IconButton
-                        size='sm'
-                        color='red'
-                        variant='text'
-                        className='!absolute top-4 right-4 rounded-full'
-                      >
-                        <HeartIcon className='h-6 w-6' />
-                      </IconButton>
-                    </CardHeader>
-                    <CardBody>
-                      <div className='mb-3 flex items-center justify-between'>
-                        <Typography variant='h5' color='blue-gray' className='font-medium'>
-                          Wooden House, Florida
-                        </Typography>
-                        <Typography
-                          color='blue-gray'
-                          className='flex items-center gap-1.5 font-normal'
-                        >
-                          <StarIcon className='-mt-0.5 h-5 w-5 text-yellow-700' />
-                          5.0
-                        </Typography>
-                      </div>
-                      <Typography color='gray'>
-                        Enter a freshly updated and thoughtfully furnished peaceful home
-                        surrounded by ancient trees, stone walls, and open meadows.
-                      </Typography>
-
-                    </CardBody>
-                    <CardFooter className='pt-3'>
-                      <Button size='lg' fullWidth>
-                        Reserve
-                      </Button>
-                    </CardFooter>
-                  </Card>
-
-                  <Card className='w-full max-w-[22rem] max-h-[35rem] overflow-auto shadow-lg'>
-                    <CardHeader floated={false} color='blue-gray'>
-                      <img
-                        src='https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-                        alt='ui/ux review check'
-                      />
-                      <div className='to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 ' />
-                      <IconButton
-                        size='sm'
-                        color='red'
-                        variant='text'
-                        className='!absolute top-4 right-4 rounded-full'
-                      >
-                        <HeartIcon className='h-6 w-6' />
-                      </IconButton>
-                    </CardHeader>
-                    <CardBody>
-                      <div className='mb-3 flex items-center justify-between'>
-                        <Typography variant='h5' color='blue-gray' className='font-medium'>
-                          Wooden House, Florida
-                        </Typography>
-                        <Typography
-                          color='blue-gray'
-                          className='flex items-center gap-1.5 font-normal'
-                        >
-                          <StarIcon className='-mt-0.5 h-5 w-5 text-yellow-700' />
-                          5.0
-                        </Typography>
-                      </div>
-                      <Typography color='gray'>
-                        Enter a freshly updated and thoughtfully furnished peaceful home
-                        surrounded by ancient trees, stone walls, and open meadows.
-                      </Typography>
-
-                    </CardBody>
-                    <CardFooter className='pt-3'>
-                      <Button size='lg' fullWidth>
-                        Reserve
-                      </Button>
-                    </CardFooter>
-                  </Card>
-                </div>
               </div>
             </div>
           </div>
