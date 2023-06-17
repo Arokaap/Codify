7a201666b03fe8122112b256c862ed327408f383
Aaron Aragon <aaron.david.nagore@gmail.com>
2023-06-17 17:43:27 +0200

    fix(routes): Routes to files fixed.

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -23,11 +23,11 @@ export function Profile ({ handleUser }) {
 
   const totalCourses = (user?.enrolledCourses.length || 0) + (user?.createdCourses.length || 0)
 
-  if (!user) {
+  if (!userLogged) {
     return (
       <>
         <section className='relative block h-[50vh]'>
-          <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('/img/background-1.jpg')] bg-cover bg-center" />
+          <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('https://res.cloudinary.com/dpew4mitl/image/upload/v1687014229/background-1_ecfeyg.jpg')] bg-cover bg-center" />
           <div className='absolute top-0 h-full w-full bg-black/75 bg-cover bg-center' />
         </section>
         <section className='relative bg-blue-gray-50/50 py-16 px-4'>
